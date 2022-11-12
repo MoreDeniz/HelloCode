07e1941566def30b4f244fac41d3b8ac41bc9f07
Mayya <msalosina@mail.ru>
2022-11-12 22:19:27 +0700

    add Ex013_1_Factorial

---
+++ Ex013_1_Factorial/Program.cs
@@ -0,0 +1,16 @@
+﻿// Factorial
+
+Console.Clear();
+
+double Factorial(int n)
+{
+    // 1! = 1
+    // 0! = 0
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
