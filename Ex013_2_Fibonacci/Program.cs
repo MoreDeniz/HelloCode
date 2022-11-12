ec6809b6e13bdda85f979a71f04c9c2e57ea0a06
Mayya <msalosina@mail.ru>
2022-11-12 22:20:17 +0700

    add Ex013_2_Fibonacci

---
+++ Ex013_2_Fibonacci/Program.cs
@@ -0,0 +1,17 @@
+﻿// Fibonacci
+
+Console.Clear();
+
+double Fibonacci(int n)
+{
+    // 1! = 1
+    // 0! = 0
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for (int i = 1; i < 30; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
+
